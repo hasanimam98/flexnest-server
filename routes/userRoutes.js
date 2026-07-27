f9f5efc85ef4396ebe0b6bc1db7bf0b8b5f043fc
hasanimam98 <hasanimam.ict@gmail.com>
2026-07-27 03:21:06 -0400

    user API

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,47 +1,155 @@
 const express = require("express");
+
 const router = express.Router();
 
 const { getDB } = require("../config/db");
 
 
-router.post("/", async (req, res) => {
 
-  try {
+// Save User after Firebase Register
+
+router.post("/", async (req,res)=>{
+
+
+  try{
+
 
     const user = req.body;
+
 
     const db = getDB();
 
 
+
+    const existingUser = await db
+      .collection("users")
+      .findOne({
+        email:user.email
+      });
+
+
+
+    if(existingUser){
+
+      return res.send({
+
+        success:true,
+
+        message:"User already exists"
+
+      });
+
+    }
+
+
+
+
+
+    const newUser = {
+
+
+      name:user.name,
+
+      email:user.email,
+
+      image:user.image || "",
+
+      role:"user",
+
+      status:"active",
+
+
+    };
+
+
+
+
     const result = await db
       .collection("users")
-      .insertOne(user);
+      .insertOne(newUser);
+
+
 
 
     res.send({
 
-      success: true,
+      success:true,
 
-      message: "User saved successfully",
+      message:"User created successfully",
 
-      insertedId: result.insertedId
+      insertedId:result.insertedId
+
 
     });
 
 
-  } catch(error) {
+
+  }catch(error){
+
 
     res.status(500).send({
 
-      success: false,
+      success:false,
 
-      message: error.message
+      message:error.message
 
     });
 
+
   }
 
+
 });
+
+
+
+
+
+
+
+// Get User by Email
+
+router.get("/:email", async(req,res)=>{
+
+
+  try{
+
+
+    const email = req.params.email;
+
+
+    const db = getDB();
+
+
+
+    const user = await db
+      .collection("users")
+      .findOne({
+        email
+      });
+
+
+
+    res.send(user);
+
+
+
+  }catch(error){
+
+
+    res.status(500).send({
+
+      message:error.message
+
+    });
+
+
+  }
+
+
+});
+
+
 
 
 module.exports = router;
