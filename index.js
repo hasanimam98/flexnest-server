deb6316c5e08a87e18bc257567e0069fe1574ed4
hasanimam98 <hasanimam.ict@gmail.com>
2026-07-27 03:08:06 -0400

    index udate

--- index.js
+++ index.js
@@ -8,20 +8,28 @@ const { connectDB } = require("./config/db");
 
 const userRoutes = require("./routes/userRoutes");
 const bookingRoutes = require("./routes/bookingRoutes");
+const postRoutes = require("./routes/postRoutes");
 
 
 const app = express();
 
 
 app.use(cors());
+
 app.use(express.json());
+
 app.use(cookieParser());
 
 
+
 // Routes
+
 app.use("/users", userRoutes);
 
 app.use("/bookings", bookingRoutes);
+
+app.use("/posts", postRoutes);
+
 
 
 
@@ -33,6 +41,7 @@ app.get("/", (req, res) => {
 
 
 
+
 const PORT = process.env.PORT || 5000;
 
 
@@ -41,10 +50,14 @@ connectDB()
 
 .then(()=>{
 
+
   app.listen(PORT, ()=>{
+
 
     console.log(`Server running on port ${PORT}`);
 
+
   });
+
 
 });
